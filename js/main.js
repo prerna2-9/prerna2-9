0fb80389a0ceffc8a649347d5a2de389ecae4b9c
Daggron <returnofking04@gmail.com>
2019-11-03 01:19:02 +0530

    Two More Pages added

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@
   particlesJS.load('particles-js', './js/particles.json', function() {
     console.log('callback - particles.js config loaded');
   });
+
+
+  new hoverEffect({
+    parent:document.querySelector(".distortion"),
+    intensity:0.2,
+    angle: 180,
+    speedIn:2.5,
+    speedOut:2.5,
+    image1:"./images/prerna.jpg",
+    image2:"./images/prerna2.jpg",
+    displacementImage:"./images/stripe.jpg"
+  })
---
+++ js/query.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){       
+    var scroll_pos = 0;
+    $(document).scroll(function() { 
+        scroll_pos = $(this).scrollTop();
+        if(scroll_pos > 210) {
+            $('.has-animation').each(function(index) {
+                $(this).delay($(this).data('delay')).queue(function(){
+                  $(this).addClass('animate-in');
+                });
+            });
+        } else {
+            $('.has-animation').each(function(index) {
+                $(this).delay($(this).data('delay')).queue(function(){
+                  $(this).addClass('animate-in');
+                });
+            });
+        }
+
+        if(scroll_pos>1500){
+            $('#skills').addClass('dark');
+            $('.sk').addClass('yellow-gold');
+            $('.sk').removeClass('text-dark');
+        }
+        else{
+            $('#skills').removeClass('dark');
+            $('.sk').removeClass('yellow-gold');
+           
+        }
+    });
+});
+

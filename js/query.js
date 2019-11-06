e07a1c9c88512ed07d3fec3a78390aa1fb07f5b1
Prerna2-9 <bajajprerna0@gmail.com>
2019-11-06 23:13:31 +0530

    Done

--- js/query.js
+++ js/query.js
@@ -2,20 +2,7 @@ $(document).ready(function(){
     var scroll_pos = 0;
     $(document).scroll(function() { 
         scroll_pos = $(this).scrollTop();
-        if(scroll_pos > 210) {
-            $('.has-animation').each(function(index) {
-                $(this).delay($(this).data('delay')).queue(function(){
-                  $(this).addClass('animate-in');
-                });
-            });
-        } else {
-            $('.has-animation').each(function(index) {
-                $(this).delay($(this).data('delay')).queue(function(){
-                  $(this).addClass('animate-in');
-                });
-            });
-        }
-
+    
         if(scroll_pos>1500){
             $('#skills').addClass('dark');
             $('.sk').addClass('yellow-gold');
